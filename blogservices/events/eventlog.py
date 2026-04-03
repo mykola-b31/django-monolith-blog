c5e6d8e5313aa69f27140548b3cee55d2adb266f
Mykola Babko <babkokolya81@gmail.com>
2026-04-03 12:45:53 +0300

    move RabbitMQ connection to entry point

--- blogservices/events/eventlog.py
+++ blogservices/events/eventlog.py
@@ -19,28 +19,6 @@ mongo_client = pymongo.MongoClient(f"mongodb://{mongo_server}/",
                                    password=os.environ['MONGO_PASS'])
 events_db = mongo_client[os.environ['EVENT_LOG_DB']]
 
-connection = None
-for i in range(10):
-    try:
-        connection =pika.BlockingConnection(
-            pika.ConnectionParameters(
-                host=os.environ['AMQP_HOST'],
-                credentials=pika.credentials.PlainCredentials(
-                    url_unquote(os.environ['AMQP_USER']),
-                    url_unquote(os.environ['AMQP_PASS'])
-                ),
-            )
-        )
-        logger.info("Connected to RabbitMQ")
-        break
-    except pika.exceptions.AMQPConnectionError:
-        logger.info(f"Failed to connect to RabbitMQ, attempt {i+1}/10")
-        time.sleep(3)
-else:
-    raise Exception("Failed to connect to RabbitMQ after 10 attempts")
-
-channel = connection.channel()
-
 def event_store(ch, method, properties, body):
     message = json.loads(body.decode())
     event_type = message.get("body", {}).get("event", "UNKNOWN_EVENT")
@@ -50,6 +28,28 @@ def event_store(ch, method, properties, body):
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 if __name__ == '__main__':
+    connection = None
+    for i in range(10):
+        try:
+            connection = pika.BlockingConnection(
+                pika.ConnectionParameters(
+                    host=os.environ['AMQP_HOST'],
+                    credentials=pika.credentials.PlainCredentials(
+                        url_unquote(os.environ['AMQP_USER']),
+                        url_unquote(os.environ['AMQP_PASS'])
+                    ),
+                )
+            )
+            logger.info("Connected to RabbitMQ")
+            break
+        except pika.exceptions.AMQPConnectionError:
+            logger.info(f"Failed to connect to RabbitMQ, attempt {i + 1}/10")
+            time.sleep(3)
+    else:
+        raise Exception("Failed to connect to RabbitMQ after 10 attempts")
+
+    channel = connection.channel()
+
     channel.basic_consume(queue=os.environ['EVENT_QUEUE'], on_message_callback=event_store)
     logger.info('[*] EventLogging service started. To exit press CTRL+C')
     channel.start_consuming()
